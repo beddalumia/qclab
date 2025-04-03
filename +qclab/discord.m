f39789f6311634489d904aae10251994bd2c82f9
Gabriele Bellomia <gbellomi@sissa.it>
2025-04-03 19:56:54 +0200

    Introducing Taboo Search
    The idea [1] is to run the random walk many times, and each time compare
    the obtained candidate minimum Q, with the previous best-candidate. When
    the new candidate is lower it becomes the new best, otherwise we drop it
    and we push the corresponding minimizer RDM to a black-list. All walks,
    except the first one, check at all steps whether the updated chi is in
    the black-list. If so, is is treated as if its "energy cost" is infinite.
    In this way we avoid at later walks to fall into black-listed false minima.
    
    Some details:
    
    - How to check if the given chi is "close" to a black-listed minimizer?
      > We compute both S(chi||taboo) and S(taboo||chi) and check if any of
        the two is "small". In principle we could just use norm(chi-taboo),
        but I guess we should base everything on the same "geometry".
    
    - Are all walks equivalent?
      > Well, in principle we want to differentiate them, to make the taboo
        search really effective in balancing local search properties with a
        good global exploration. Yet I found that changing starting point,
        as in starting from a Haar-measure pair of unitaries instead of the
        identity, causes a lot of pain, at least for the usual data for the
        local RDMs across the Mott transition. In general changing starting
        point should be very important, yet. I don't know.
      > For now we change only the "step length" parameter, to be sure that
        at least a subset of the walks has good acceptance ratio. The local
        RDMs look to be well under control (no "outliers" in the final data
        and the classical correlation is exactly superimposed to the mutual
        information; the discord is always well under 1e-4).
    
    - Does this work for the two-site RDMs (across the MIT)?
      > Not sure what to think about it... the classical correlations look
        very very smooth and match very well doing two independent runs of
        the script, across all CDMFT points. Yet, the quantum discord looks
        off, as a) it fluctuates a lot and b) it violates the usual sum rule
        I ≥ Q + C. It might be that using different starting points is in
        fact a must, for states that are not already pseudoclassical. I mean,
        it would make sense. I'll try it...
    
    [1] https://en.wikipedia.org/wiki/Tabu_search

--- +qclab/discord.m
+++ +qclab/discord.m
@@ -3,7 +3,7 @@ function [Q,chi,C] = discord(rho,A,B)
 	%  in the information geometry framework, assuming a
 	%  metric induced by the Quantum Relative Entropy.
 	%
-	%  [Q,C,chi] = discord(rho)
+	%  [Q,chi,C] = discord(rho,A,B)
 	%
 	%  with
 	%
@@ -21,6 +21,7 @@ function [Q,chi,C] = discord(rho,A,B)
 	%  Copyright: Gabriele Bellomia, 2025
 	%  
 	%  Ref: Lexin Ding et al 2023 Quantum Sci. Technol. 8 015015
+	%       (but we implement some additional tricks to minimize)
 
 	N = size(rho,1); 
 	if mod(N,4)~=0
@@ -34,100 +35,179 @@ function [Q,chi,C] = discord(rho,A,B)
 	end
 
 	Nr = sqrt(N); % Reduced System Dimension
+	Nc = 50; % Number of candidates
 
-	% Initial guess for the local unitaries
-	Ua = eye(Nr);  
-	Ub = eye(Nr);
+	taboos = {};
+	q_best = NaN;
+	for instance = 1:Nc
 
-    % Initial guess for the chi
-    chi = zeros(N,N);
-    for i = 0:Nr-1
-    	for j = 0:Nr-1
-    		ixj = 1 + i + j*Nr;
-    		chi(ixj,ixj) = rho(ixj,ixj);
-    	end
-    end
-	try 
-		qclab.math.is_rdm(chi);
-	catch
-		error("The initial guess for the closest pseudo-classical state is not a valid density matrix")
-	end
+        fprintf("Starting the walk #%d\n",instance)
 
-    % Initial guess for the Discord
-    Q = qclab.rentropy(rho,chi);
+		% Initializations
+		chi = zeros(N,N);
+		M = 100*instance; beta = 1;
 
-    M = 1000; beta = 1;
-    t = 0; n = 0;
-    converged = false;
-    chi_ = zeros(N,N);
+		% Initial guess for the local unitaries
+        % Always try the identity (other Haar starting points apparently
+        %                          suck... to be investigated why)
+        Ua = eye(Nr);  
+        Ub = eye(Nr);
 
-    converged = false;
-    while not(converged)
-    	% Sample local unitary matrices
-    	Va = qclab.math.random_unitary(Nr);
-    	Vb = qclab.math.random_unitary(Nr);
-    	% Constrain them to be close to eye
-    	dVa = mpower(Va,1/M); 
-    	dVb = mpower(Vb,1/M); 
-    	% Update local bases
-    	Ua_ = dVa*Ua;
-    	Ub_ = dVb*Ub;
-    	% Map the density matrix to the new basis
-    	Uaxb = kron(Ua_,Ub_);
-    	rho_ = Uaxb*rho*Uaxb';
-    	% Update guess for chi in the new basis
-	    for i = 0:Nr-1
-    		for j = 0:Nr-1
-			    ixj = 1 + i + j*Nr;
-    			chi_(ixj,ixj) = rho_(ixj,ixj);
-    		end
-    	end
+		% Initial guess for the chi
+		for i = 0:Nr-1
+			for j = 0:Nr-1
+				ixj = 1 + i + j*Nr;
+				chi(ixj,ixj) = rho(ixj,ixj);
+			end
+        end
+
 		try 
-			qclab.math.is_rdm(chi_);
+			qclab.math.is_rdm(chi);
 		catch
-			error("The updated candidate closest pseudo-classical state is not a valid density matrix")
+			error("The initial guess for the closest pseudo-classical state is not a valid density matrix")
 		end
-    	% Rotate back to the computational basis
-    	% chi_ = Uaxb'*chi_*Uaxb; % DOING IT HERE CAUSES ALL SORTS OF NUMERICAL ISSUES
-    	% Update guess for the Quantum Discord
-    	Q_ = qclab.rentropy(rho_,chi_);
-    	% Thermal shananingans
-    	p = rand;
-    	if exp(-beta*(Q_-Q)) > p
-	    	% Convergence criteria
-	    	if abs(Q_-Q)<1E-7
-	    		converged = true;
-	    	end
-	    	if n > 10^4
-	    		converged = true;
-	    	end
-    		Q = Q_;
-    		chi = Uaxb'*chi_*Uaxb; % (rotate back to original basis)
-    		Ua = Ua_;
-    		Ub = Ub_;
-    		n = n + 1;
-    	end
-    	t = t + 1;
-    	beta = beta + 100; % Schedule :|
-    	if mod(t,100) == 0
-    		fprintf("Time step #%d\n---------------------\n",t)
-    		fprintf("Annealing temperature: T = %.2E\n",1/beta)
- 			fprintf("Total acceptance rate: %f%%\n\n", n/t*100)
-    	end
+
+		% Initial guess for the Discord
+		Q = qclab.rentropy(rho,chi);
+        
+
+		% % In general try a random Haar-measure unitary
+		% Ua = qclab.math.random_unitary(Nr);
+		% Ub = qclab.math.random_unitary(Nr);
+        % %
+		% % Map the density matrix to the new basis
+		% Uaxb = kron(Ua,Ub);
+		% rho_ = Uaxb*rho*Uaxb';
+        % %
+		% % Update guess for chi in the new basis
+		% for i = 0:Nr-1
+		%  	for j = 0:Nr-1
+		%  		ixj = 1 + i + j*Nr;
+		%  		chi(ixj,ixj) = rho_(ixj,ixj);
+		%  	end
+		% end
+		% try 
+		% 	qclab.math.is_rdm(chi);
+		% catch
+		%  	error("The updated candidate closest pseudo-classical state is not a valid density matrix")
+		% end
+        % % 
+		% % % Initial guess for the Discord
+		% Q = qclab.rentropy(rho_,chi);
+        % % 
+		% % % Rotate back to the computational basis
+		% chi = Uaxb'*chi*Uaxb;
+
+		converged = false;
+		t = 0; n = 0;
+		while not(converged)
+			% Sample local unitary matrices
+			Va = qclab.math.random_unitary(Nr);
+			Vb = qclab.math.random_unitary(Nr);
+			% Constrain them to be close to eye
+			dVa = mpower(Va,1/M); 
+			dVb = mpower(Vb,1/M); 
+			% Update local bases
+			Ua_ = dVa*Ua;
+			Ub_ = dVb*Ub;
+			% Map the density matrix to the new basis
+			Uaxb = kron(Ua_,Ub_);
+			rho_ = Uaxb*rho*Uaxb';
+			% Update guess for chi in the new basis
+			for i = 0:Nr-1
+				for j = 0:Nr-1
+					ixj = 1 + i + j*Nr;
+					chi_(ixj,ixj) = rho_(ixj,ixj);
+				end
+			end
+			try 
+				qclab.math.is_rdm(chi_);
+			catch
+				error("The updated candidate closest pseudo-classical state is not a valid density matrix")
+			end
+			% Rotate back to the computational basis
+			% chi = Uaxb'*chi_*Uaxb; % DOING IT HERE CAUSES ALL SORTS OF NUMERICAL ISSUES
+			% Update guess for the Quantum Discord
+			Q_ = qclab.rentropy(rho_,chi_);
+			% Rotate back to the computational basis
+			chi = Uaxb'*chi_*Uaxb; 
+			% Determine if the new chi is in the taboo list
+			taboo = false;
+			Dl = zeros(1,numel(taboos));
+			Dr = zeros(1,numel(taboos));
+			for i = 2:numel(taboos)
+				Dl(i) = qclab.rentropy(chi,taboos{i}); % Rel. Entropy is 
+				Dr(i) = qclab.rentropy(taboos{i},chi); % noncommutative!
+			end
+			taboo = any(Dl(i)<1e-8) || any(Dr(i)<1e-8);
+			if taboo
+				% We discard as if very high
+                fprintf("TABOO: let's avoid entering a known false minimum\n")
+				break
+			else
+				% Thermal shenanigans
+				p = rand;
+				if exp(-beta*(Q_-Q)) > p
+					Q = Q_;
+					Ua = Ua_;
+					Ub = Ub_;
+					n = n + 1;
+					% Convergence criteria
+					if abs(Q_-Q)<1E-7
+						converged = true;
+                        fprintf("Exiting walk #%d as we are converged!\n",instance)
+					end
+					if n > 10^4
+						converged = true;
+                        fprintf("Exiting walk #%d after too many steps!\n",instance)
+					end
+					if converged 
+					% Taboo-enhanced criteria
+						if q_best<Q
+							% We have already a better candidate
+							% so we discard Q and flag chi as a
+							% taboo state
+                            fprintf("TABOO: found a false minimum!\n")
+                            fprintf("       > discarding the walk.\n")
+							taboos = {taboos,chi};
+                            break
+							converged = false;
+                            t = 0; n = 0;
+						else
+							% The new converged Q is a good candidate,
+							% so we do not update the taboo list and
+							% we record the new best candidate
+							q_best = Q;
+                            chi_best = chi;
+							converged = true;
+						end	
+					end
+				end
+			end
+			t = t + 1;
+			beta = beta + 100; % Schedule :|
+			if mod(t,100) == 0
+				fprintf("Time step #%d\n---------------------\n",t)
+				fprintf("Annealing temperature: T = %.2E\n",1/beta)
+				fprintf("Total acceptance rate: %f%%\n\n", n/t*100)
+			end
+		end
+
 	end
 
 	% Once we finish, we can compute the classical correlations
 	% contained in the candidate closest pseudo-classical state
-	C = qclab.mutinfo(chi,A,B); % More reliable for diagonal rho
-	c = qclab.rentropy(chi,kron(A,B));
+	C = qclab.mutinfo(chi_best,A,B); % More reliable for diagonal rho
+	c = qclab.rentropy(chi_best,kron(A,B));
 	if abs(C-c)>1E-12
 		warning("The closest pseudo-classical state might be pathological.");
 		fprintf("χ* = \n\n")
-		disp(chi)
-		qclab.math.is_rdm(chi);
+		disp(chi_best)
+		qclab.math.is_rdm(chi_best);
 	end
 
 	% Final sanity check and return
+    Q = q_best;
 	assert(abs(imag(Q))<1E-12,"Imaginary discord!"); Q = real(Q);
 	assert(abs(imag(C))<1E-12,"Imaginary classical correlations!"); C = real(C);
 
